e9ff4d08df137e25f60d08c9496c0a4b5f14451d
Meldrid-9124 <maarijmoeen12345@gmail.com>
2025-12-02 11:16:17 +0500

    Added three python function files

---
+++ multiply.py
@@ -0,0 +1,6 @@
+def multiply(num1, num2):
+    return num1*num2;
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+product = multiply(num1, num2)
+print(f"The multiplication of the numbers {num1} and {num2} is {product}")
---
+++ rename_numbers.txt
@@ -0,0 +1,4 @@
+def greet(name):
+    print(f"Hello {name}, how are you doing?")
+name = input("Enter your name: ")
+greet(name)
